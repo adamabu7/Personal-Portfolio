87699dc4e32d2f60520de87f258be42354a886ad
Adam <adamabu7@gmail.com>
2025-02-24 00:49:27 -0500

    Create columns, image and description

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/Capture1.png", width= 400)
+
+with col2:
+    st.title("Adam Abu-Shtayyah")
+    content = """
+    Hi, I am Adam! I am a Python programmer student. I graduated from University of Michigan-Dearborn in 2020 with a BSME in Mechanical Engineering.
+    """
+    st.info(content)
